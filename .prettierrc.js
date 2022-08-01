720259e2a4b20649a2427dde03345c4b660200fc
AlexFreik <freik.aa@phystech.edu>
2022-08-01 20:06:50 +0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: false,
+	semi: true,
+	trailingComma: "all",
+	arrowParens: "always",
+	overrides: [],
+};
---
+++ Makefile
@@ -0,0 +1,16 @@
+.PHONY: *
+
+build:
+	npm run build
+
+dev:
+	npm run dev
+
+pretty:
+	npx prettier --write .
+
+next-lint:
+	npx next lint
+
+lint:
+	npx lint-staged
